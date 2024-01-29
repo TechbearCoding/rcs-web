8aacb31adfdb3f01094fbc599335850f73844850
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2024-01-29 21:11:17 +0200

    samples

--- day9-lists/src/app/string-list/string-list.component.ts
+++ day9-lists/src/app/string-list/string-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-string-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './string-list.component.html',
   styleUrl: './string-list.component.css'
 })
 export class StringListComponent implements OnInit{
   
+  index: number = -1;
+  studentFirstname: string = "";
+
   lst: Student[] = [
     {
       firstName: "Janis",
@@ -35,6 +39,14 @@ export class StringListComponent implements OnInit{
 
   ngOnInit(): void {
     console.log("aaa1");
+  }
+
+  replaceName() : void{
+    if(this.index > 0 && this.index < this.lst.length){
+      this.lst[this.index].firstName = this.studentFirstname;
+    }else{
+      alert("Nepareiza ievade");
+    } 
   }
 
 }
